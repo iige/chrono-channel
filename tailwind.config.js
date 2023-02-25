b2d4a0da851cc823605b6fc001ebbd0ce04918be
Isaac Ige <isaac_ige9@yahoo.com>
2023-02-25 15:19:31 -0800

    Add Twitch's recommended colors to Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkPurple: "#201C2B",
+        purpleAccent: "#6441A4",
+        lightGrey: "#E5E3E8",
+        lightPurple: "#E2DBF0"
+      }
+    },
   },
   plugins: [],
 }

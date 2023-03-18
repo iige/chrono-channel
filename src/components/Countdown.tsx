fbb599c904ce4450f692d4c0f62fb851d8ff5dc7
Isaac Ige <isaac_ige9@yahoo.com>
2023-03-18 16:15:51 -0700

    Load game image as background if applicable

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,47 +3,28 @@ import { DaysOfTheWeek } from "./DaysOfTheWeek";
 import { Header } from "./Header";
 import { TimeDisplay } from "./TimeDisplay";
 import { Timezone } from "./Timezone";
-
-// Type for a stream schedule "segment" as noted here: https://dev.twitch.tv/docs/api/reference/#get-channel-stream-schedule
-type Segment = {
-  id: string;
-  start_time: string;
-  end_time: string;
-  title: string;
-  category: {
-    id: string;
-    name: string;
-  };
-  canceled_until: string;
-  is_recurring: boolean;
-};
-
-type ScheduleApiResponseData = {
-  data: {
-    broadcaster_id: string;
-    broadcaster_name: string;
-    broadcaster_login: string;
-    segments: Segment[];
-    vacation: {
-      start_time: string;
-      end_time: string;
-    } | null;
-  };
-  pagination: {
-    cursor?: string;
-  };
-};
+import {
+  CategoryApiResponseData,
+  ScheduleApiResponseData,
+  Segment,
+} from "./types";
 
 type CountdownState = {
   firstMount: boolean;
   scheduleData: ScheduleApiResponseData | null;
+  categoryData: CategoryApiResponseData | null;
   nextStream: Segment | null;
 };
 
 export class Countdown extends React.Component<{}, CountdownState> {
   constructor(props: {}) {
     super(props);
-    this.state = { firstMount: true, scheduleData: null, nextStream: null };
+    this.state = {
+      firstMount: true,
+      scheduleData: null,
+      categoryData: null,
+      nextStream: null,
+    };
   }
 
   componentDidUpdate(
@@ -54,27 +35,35 @@ export class Countdown extends React.Component<{}, CountdownState> {
     if (this.state.firstMount !== prevState.firstMount) {
       this.fetchData();
     }
+    console.log(this.state);
   }
 
   async fetchData(): Promise<void> {
     console.log("fetching data");
 
-    const updateScheduleData = (response: ScheduleApiResponseData) => {
-      const nextStream =
-        response.data.segments.length > 0 ? response.data.segments[0] : null;
-      this.setState({
-        ...this.state,
-        scheduleData: response,
-        nextStream: nextStream,
+    const updateScheduleData = (
+      response: ScheduleApiResponseData,
+      nextStream: Segment | null
+    ) => {
+      this.setState((prevState) => {
+        return { ...prevState, scheduleData: response, nextStream: nextStream };
       });
       console.log("schedule data", response);
+    };
+
+    const updateCategoryData = (response: CategoryApiResponseData) => {
+      this.setState((prevState) => {
+        return { ...prevState, categoryData: response };
+      });
+      console.log("category data", response);
     };
 
     (window as any).Twitch.ext.onAuthorized(async function (auth: any) {
       console.log("The Helix JWT is ", auth.helixToken);
       console.log(auth);
 
-      const response = await fetch(
+      // Fetch the upcoming stream schedule for the channel
+      const scheduleResponse = await fetch(
         `https://api.twitch.tv/helix/schedule?broadcaster_id=${auth.channelId}`,
         {
           headers: {
@@ -84,8 +73,28 @@ export class Countdown extends React.Component<{}, CountdownState> {
         }
       );
 
-      const responseJson = await response.json();
-      updateScheduleData(responseJson);
+      const scheduleResponseJson = await scheduleResponse.json();
+      const nextStream =
+        scheduleResponseJson.data.segments.length > 0
+          ? scheduleResponseJson.data.segments[0]
+          : null;
+
+      // If there is an upcoming stream, fetch the category data for it
+      if (nextStream?.category?.id) {
+        const categoryResponse = await fetch(
+          `https://api.twitch.tv/helix/games?id=${nextStream.category.id}`,
+          {
+            headers: {
+              "Client-Id": `${auth.clientId}`,
+              Authorization: `Extension ${auth.helixToken}`,
+            },
+          }
+        );
+        const categoryResponseJson = await categoryResponse.json();
+        updateCategoryData(categoryResponseJson);
+      }
+
+      updateScheduleData(scheduleResponseJson, nextStream);
     });
   }
 
@@ -95,17 +104,33 @@ export class Countdown extends React.Component<{}, CountdownState> {
 
   render(): ReactNode {
     let timeDisplay = <></>;
+    let categoryUrl = "";
+    let contentStyle: React.CSSProperties = {};
+
     if (this.state.nextStream) {
       timeDisplay = (
         <TimeDisplay streamStartTime={this.state.nextStream.start_time} />
       );
     }
+
+    if (this.state.categoryData) {
+      categoryUrl = this.state.categoryData.data[0].box_art_url
+        .replace("{width}", "270")
+        .replace("{height}", "360");
+      console.log("categoryUrl: " + categoryUrl);
+      contentStyle.background = `linear-gradient(rgba(32,28,43,0.8), rgba(32,28,43,0.8)), url(${categoryUrl}) center / contain no-repeat`;
+    }
+
     return (
       <div className="flex min-h-full flex-col">
         <Header
           channelName={this.state.scheduleData?.data.broadcaster_name ?? ""}
         />
-        <div className="grow bg-twitchDarkPurple">
+        <div
+          className="grow bg-twitchDarkPurple"
+          id="extensionContent"
+          style={contentStyle}
+        >
           {timeDisplay}
           <DaysOfTheWeek></DaysOfTheWeek>
           <Timezone></Timezone>
---
+++ src/components/types.tsx
@@ -0,0 +1,38 @@
+// Type for a stream schedule "segment" as noted here: https://dev.twitch.tv/docs/api/reference/#get-channel-stream-schedule
+export type Segment = {
+  id: string;
+  start_time: string;
+  end_time: string;
+  title: string;
+  category: {
+    id: string;
+    name: string;
+  };
+  canceled_until: string;
+  is_recurring: boolean;
+};
+
+export type ScheduleApiResponseData = {
+  data: {
+    broadcaster_id: string;
+    broadcaster_name: string;
+    broadcaster_login: string;
+    segments: Segment[];
+    vacation: {
+      start_time: string;
+      end_time: string;
+    } | null;
+  };
+  pagination: {
+    cursor?: string;
+  };
+};
+
+export type CategoryApiResponseData = {
+  data: {
+    id: string;
+    name: string;
+    box_art_url: string;
+    igdb_id: number;
+  }[];
+};

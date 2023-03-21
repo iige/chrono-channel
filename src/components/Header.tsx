57cc61a32da9c08354835bdd365c9740e9ac8ef2
Isaac Ige <isaac_ige9@yahoo.com>
2023-03-20 21:26:20 -0700

    Adjust header text if channel name cannot be obtained

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,13 @@ type HeaderProps = {
 
 export class Header extends React.Component<HeaderProps, {}> {
   render() {
+    let headerText = "Next Stream";
+    if (this.props.channelName) {
+      headerText = `${this.props.channelName}'s Next Stream`;
+    }
     return (
       <div className="bg-twitchPurple">
-        <h1 className="p-3 text-center uppercase text-white">
-          {this.props.channelName}'s Next Stream
-        </h1>
+        <h1 className="p-3 text-center uppercase text-white">{headerText}</h1>
       </div>
     );
   }

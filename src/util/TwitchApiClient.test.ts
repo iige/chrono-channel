70b7a135d8ec5183ae6d54480f95132e9efeb236
Isaac Ige <isaac_ige9@yahoo.com>
2023-03-21 20:53:41 -0700

    Create tests for TwitchApiClient

--- src/util/TwitchApiClient.test.ts
+++ src/util/TwitchApiClient.test.ts
@@ -1,69 +1,71 @@
 import { TwitchApiClient } from "./TwitchApiClient";
 
+/**
+ * Note that Create React App sets 'resetMocks' to true by for Jest by default.
+ * This means that any mocks you create will be reset between tests.
+ * This was causing issues with manually mocking 'fetch' calls mock, so I've set it to false in the package.json
+ *
+ * Refer to: https://stackoverflow.com/questions/68168516/why-does-my-mock-fetch-always-returns-null-jest
+ */
+
 var auth = { clientId: "test", helixToken: "test", channelId: "test" };
 const twitchClient = new TwitchApiClient(auth);
 
 describe("TwitchApiClient", () => {
+  const fetchSpy = jest.spyOn(global, "fetch") as jest.Mock;
+  fetchSpy.mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      json: jest.fn().mockResolvedValue({}),
+    })
+  );
   it("Should always fetch with the appropriate headers", async () => {
     const spy = jest.spyOn(twitchClient as any, "fetchWithAuth");
-    const fetchSpy = jest.spyOn(global, "fetch") as jest.Mock;
-    fetchSpy.mockImplementation(() =>
-      Promise.resolve({
-        ok: true,
-        json: jest.fn().mockResolvedValue({}),
-      })
-    );
     await twitchClient.getScheduleData();
     await twitchClient.getCategoryData("test");
     await twitchClient.getStreamData();
     expect(spy).toBeCalledTimes(3);
   });
 
-  it("Schedule Data: Should return null if the response is not ok", async () => {
-    const fetchSpy = jest.spyOn(global, "fetch") as jest.Mock;
-    fetchSpy.mockImplementation(() =>
-      Promise.resolve({
-        ok: false,
-        json: () => jest.fn().mockResolvedValue({}),
-      })
-    );
+  it("Schedule Data: Should return null if the fetch status is not ok", async () => {
+    fetchSpy.mockImplementation(() => Promise.resolve({ ok: false }));
     const result = await twitchClient.getScheduleData();
     expect(result).toBeNull();
   });
 
-  it("Schedule Data: Returns the json if the response is ok", async () => {
-    const fetchSpy = jest.spyOn(global, "fetch") as jest.Mock;
+  it("Schedule Data: Should return an object if the fetch status is ok", async () => {
     fetchSpy.mockImplementation(() =>
-      Promise.resolve({
-        ok: true,
-        json: () => jest.fn().mockResolvedValue({}),
-      })
+      Promise.resolve({ ok: true, json: jest.fn().mockResolvedValue({}) })
     );
     const result = await twitchClient.getScheduleData();
     expect(result).not.toBeNull();
   });
 
-  it("Category Data: Should return null if the response is not ok", async () => {
-    const fetchSpy = jest.spyOn(global, "fetch") as jest.Mock;
-    fetchSpy.mockImplementation(() =>
-      Promise.resolve({
-        ok: false,
-        json: () => jest.fn().mockResolvedValue({}),
-      })
-    );
+  it("Category Data: Should return null if the fetch status is not ok", async () => {
+    fetchSpy.mockImplementation(() => Promise.resolve({ ok: false }));
     const result = await twitchClient.getCategoryData("test");
     expect(result).toBeNull();
   });
 
-  it("Stream Data: Should return null if the response is not ok", async () => {
-    const fetchSpy = jest.spyOn(global, "fetch") as jest.Mock;
+  it("Category Data: Should return an object if the fetch status is ok", async () => {
     fetchSpy.mockImplementation(() =>
-      Promise.resolve({
-        ok: false,
-        json: () => jest.fn().mockResolvedValue({}),
-      })
+      Promise.resolve({ ok: true, json: jest.fn().mockResolvedValue({}) })
     );
+    const result = await twitchClient.getCategoryData("test");
+    expect(result).not.toBeNull();
+  });
+
+  it("Stream Data: Should return null if the fetch status is not ok", async () => {
+    fetchSpy.mockImplementation(() => Promise.resolve({ ok: false }));
     const result = await twitchClient.getStreamData();
     expect(result).toBeNull();
+  });
+
+  it("Stream Data: Should return an object if the fetch status is ok", async () => {
+    fetchSpy.mockImplementation(() =>
+      Promise.resolve({ ok: true, json: jest.fn().mockResolvedValue({}) })
+    );
+    const result = await twitchClient.getStreamData();
+    expect(result).not.toBeNull();
   });
 });

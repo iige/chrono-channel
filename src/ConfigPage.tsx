6464195efe4e4ca516e558700e1550f16d3d7344
Isaac Ige <isaac_ige9@yahoo.com>
2023-07-27 20:16:21 -0700

    Address security risk by setting rel attribute

--- src/ConfigPage.tsx
+++ src/ConfigPage.tsx
@@ -23,7 +23,7 @@ function ConfigPage() {
     <div className="font-montserrat">
       <h1 className="text-white py-2">Configuration</h1>
       <p className="text-white">This extension automatically loads information from your schedule.</p>
-      {channelName && <a href={`https://twitch.tv/${channelName}/schedule`} target="_blank"><button className="text-white rounded-md bg-twitchPurple py-2 mt-2 px-4">Edit Schedule</button></a>}
+      {channelName && <a href={`https://twitch.tv/${channelName}/schedule`} target="_blank" rel="noreferrer"><button className="text-white rounded-md bg-twitchPurple py-2 mt-2 px-4">Edit Schedule</button></a>}
     </div>
   );
 }
